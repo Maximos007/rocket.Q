efd74ade4f2df8a0e127325e23f8d37bf98e99c1
wasley <waslleymax@gmail.com>
2021-10-15 23:50:01 -0300

    Update modal.js

--- public/script/modal.js
+++ public/script/modal.js
@@ -1,23 +1,25 @@
 export default function Modal(){
 
-	const modalWrapper = document.querySelector('.modal-wrapper')
-	const cancelButton = document.querySelector('.button.cancel')
+	// eslint-disable-next-line no-undef
+	const modalWrapper = document.querySelector('.modal-wrapper');
+	// eslint-disable-next-line no-undef
+	const cancelButton = document.querySelector('.button.cancel');
 
-	cancelButton.addEventListener("click", close)
+	cancelButton.addEventListener('click', close);
 
 	function open(){
-			//funcionalidade de atribuir a classe active para a modal
-			modalWrapper.classList.add("active")
+		//funcionalidade de atribuir a classe active para a modal
+		modalWrapper.classList.add('active');
 	}
 	function close(){
-			//funcionalidade de remover a classe active da modal
-			modalWrapper.classList.remove("active")
+		//funcionalidade de remover a classe active da modal
+		modalWrapper.classList.remove('active');
 	}
 
 	return{
-			open,
-			close
-	}
+		open,
+		close
+	};
 }
 
 // export default function Modal() {
