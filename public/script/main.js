4362946d86842c044dbc0e36c8ff25f74bed0848
wasley <waslleymax@gmail.com>
2021-10-15 23:50:03 -0300

    Update main.js

--- public/script/main.js
+++ public/script/main.js
@@ -1,45 +1,52 @@
-import Modal from './modal.js'
+import Modal from './modal.js';
 
-const modal = Modal()
+const modal = Modal();
 
-const modalTitle = document.querySelector('.modal h2')
-const modalDescription = document.querySelector('.modal p')
-const modalButton = document.querySelector('.modal button')
+// eslint-disable-next-line no-undef
+const modalTitle = document.querySelector('.modal h2');
+// eslint-disable-next-line no-undef
+const modalDescription = document.querySelector('.modal p');
+// eslint-disable-next-line no-undef
+const modalButton = document.querySelector('.modal button');
 
 
 //Pegar todos os botões que existe com a classe check
-const checkButtons = document.querySelectorAll(".actions a.check") 
+// eslint-disable-next-line no-undef
+const checkButtons = document.querySelectorAll('.actions a.check'); 
 
 checkButtons.forEach(button => {
-    //adicionar a escuta
-    button.addEventListener("click", handleClick)
-})
+	//adicionar a escuta
+	button.addEventListener('click', handleClick);
+});
 
 
 /*Quando o botão delete for clicado ele abre a modal */
-const deleteButton = document.querySelectorAll(".actions a.delete")
+// eslint-disable-next-line no-undef
+const deleteButton = document.querySelectorAll('.actions a.delete');
 
 deleteButton.forEach(button => {
-    button.addEventListener("click", (event) => handleClick(event, false))
-})
+	button.addEventListener('click', (event) => handleClick(event, false));
+});
 
 function handleClick(event, check = true){
-    event.preventDefault()
-    const text = check ? "Marcar como lida" : "Excluir"
-    const slug = check ? "check" : "delete"
-    const roomId = document.querySelector("#room-id").dataset.id
-    const questionId = event.target.dataset.id
+	event.preventDefault();
+	const text = check ? 'Marcar como lida' : 'Excluir';
+	const slug = check ? 'check' : 'delete';
+	// eslint-disable-next-line no-undef
+	const roomId = document.querySelector('#room-id').dataset.id;
+	const questionId = event.target.dataset.id;
 
-    const form = document.querySelector(".modal form")
-    form.setAttribute("action", `/question/${roomId}/${questionId}/${slug}`)
+	// eslint-disable-next-line no-undef
+	const form = document.querySelector('.modal form');
+	form.setAttribute('action', `/question/${roomId}/${questionId}/${slug}`);
 
-    modalTitle.innerHTML= `${text} esta pergunta`
-    modalDescription.innerHTML= `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`
-    modalButton.innerHTML= `Sim, ${text.toLowerCase()}`
-    check ? modalButton.classList.remove("red") : modalButton.classList.add("red")
+	modalTitle.innerHTML= `${text} esta pergunta`;
+	modalDescription.innerHTML= `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`;
+	modalButton.innerHTML= `Sim, ${text.toLowerCase()}`;
+	check ? modalButton.classList.remove('red') : modalButton.classList.add('red');
     
-    //abrir modal
-    modal.open()
+	//abrir modal
+	modal.open();
 }
 
 // import Modal  from './modal.js';
