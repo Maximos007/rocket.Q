f866884e6129584ce65732ba3898e71c26d20148
wasley <waslleymax@gmail.com>
2021-10-15 23:49:46 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -8,6 +8,8 @@ server.set('view engine', 'ejs');
 server.use(express.static('public'));
 server.set('views', path.join(__dirname, 'views'));
 
+server.use(express.urlencoded({extended: true}));
+
 server.use(route);
 
 server.listen(3333, () => console.log('live'));
